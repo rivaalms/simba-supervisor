b9b93ef19575223ac736d17b6152b84c9b5aefc3
rivaalms <rvalms@icloud.com>
2024-02-06 16:27:33 +0700

    feat(utils): base api utils

---
+++ utils/apis/index.ts
@@ -0,0 +1,58 @@
+type Options = {
+   method: 'get' | 'post' | 'put' | 'delete' | 'GET' | 'POST' | 'PUT' | 'DELETE',
+   headers?: Wildcard,
+   params?: Wildcard,
+   query?: Wildcard,
+   body?: Wildcard
+}
+
+export async function $api(url: string, opts?: { [key: string]: any }) : Promise<unknown> {
+   const baseUrl = useRuntimeConfig().public.apiBaseUrl
+   const authStore = useAuthStore()
+   const store = useAppStore()
+
+   const response = await $fetch(url, {
+      baseURL: baseUrl,
+      ...opts,
+
+      async onRequest({ options }) {
+         const headers : { [key: string]: any } = {
+            ...options.headers,
+            'Accept': 'application/json'
+         }
+
+         if (authStore.isAuthenticated) {
+            headers.Authorization = `Bearer ${authStore.getToken}`
+         }
+         options.headers = headers
+      },
+
+      async onRequestError({ error }) {
+         const { name, message } = error
+         store.notify('error', `${name}: ${message}`)
+      },
+
+      async onResponse({ response }) {
+         return response._data
+      },
+
+      async onResponseError({ response }) {
+         const { status, _data } = response
+
+         if (status == 403) {
+            store.notify('error', `${status}: Akses tidak diizinkan`)
+         }
+
+         if (status == 401) {
+            authStore.$reset()
+            localStorage.removeItem('simba-school-user')
+            localStorage.removeItem('simba-school-token')
+            navigateTo('/login')
+         }
+
+         store.notify('error', `${status}: ${_data.message}`)
+      }
+   })
+
+   return response
+}
